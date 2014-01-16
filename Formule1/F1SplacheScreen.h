2678b03d09376088853d2a20eb469be57cdb5f58
RICHARD Eddy <richard.eddy@live.fr>
2014-01-16 20:39:03 +0100

    Fichier F1SplacheScreen

---
+++ Formule1/F1SplacheScreen.h
@@ -0,0 +1,13 @@
+//
+//  F1SplacheScreen.h
+//  Formule1
+//
+//  Created by RICHARD Eddy on 16/01/2014.
+//  Copyright (c) 2014 richardeddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface F1SplacheScreen : UIView
+
+@end
---
+++ Formule1/F1SplacheScreen.m
@@ -0,0 +1,31 @@
+//
+//  F1SplacheScreen.m
+//  Formule1
+//
+//  Created by RICHARD Eddy on 16/01/2014.
+//  Copyright (c) 2014 richardeddy. All rights reserved.
+//
+
+#import "F1SplacheScreen.h"
+
+@implementation F1SplacheScreen
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
