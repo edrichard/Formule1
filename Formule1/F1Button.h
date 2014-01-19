baa3475be6a046b130f973f26e6a52415c0aeb56
RICHARD Eddy <richard.eddy@live.fr>
2014-01-19 23:21:21 +0100

    Custom view F1Button

---
+++ Formule1/F1Button.h
@@ -0,0 +1,32 @@
+//
+//  F1Button.h
+//  Formule1
+//
+//  Created by RICHARD Eddy on 19/01/2014.
+//  Copyright (c) 2014 richardeddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol F1ButtonDelegate;
+
+@interface F1Button : UIView
+
+@property (nonatomic, strong) NSString *labelName;
+@property (nonatomic, strong) NSString *icoName;
+@property (nonatomic, strong) NSString *nomBoutton;
+@property(nonatomic, weak) id<F1ButtonDelegate> delegate;
+
+- (void) setup;
+
+@end
+
+@protocol F1ButtonDelegate <NSObject>
+
+- (void) didTapEquipeButton:(F1Button *) f1Button;
+- (void) didTapCircuitButton:(F1Button *) f1Button;
+- (void) didTapActuButton:(F1Button *) f1Button;
+
+- (void) didTapInfoButton:(F1Button *) f1Button;
+
+@end
---
+++ Formule1/F1Button.m
@@ -0,0 +1,52 @@
+//
+//  F1Button.m
+//  Formule1
+//
+//  Created by RICHARD Eddy on 19/01/2014.
+//  Copyright (c) 2014 richardeddy. All rights reserved.
+//
+
+#import "F1Button.h"
+
+@interface F1Button ()
+
+- (void) didTaped:(UITapGestureRecognizer *)sender;
+
+@end
+
+@implementation F1Button
+
+@synthesize labelName = _labelName;
+@synthesize icoName = _icoName;
+@synthesize nomBoutton = _nomBoutton;
+
+- (void) setup
+{
+    UIImageView *icoView = [[UIImageView alloc] initWithFrame:CGRectMake(15, 35, 128, 128)];
+    UIImage *icoImg = [UIImage imageNamed:_icoName];
+    icoView.image = icoImg;
+    [self addSubview:icoView];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(157.5, 35, 128, 128)];
+    label.text = _labelName;
+    [self addSubview:label];
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTaped:)];
+    tapGesture.enabled = YES;
+    [self addGestureRecognizer:tapGesture];
+}
+
+- (void) didTaped:(UITapGestureRecognizer *)sender
+{
+    if ([[self delegate] respondsToSelector:@selector(didTapEquipeButton:)] && [self.nomBoutton isEqualToString: F1_BOUTTON_EQUIPE]) {
+        [self.delegate didTapEquipeButton:self];
+    } else if ([[self delegate] respondsToSelector:@selector(didTapCircuitButton:)] && [self.nomBoutton isEqualToString: F1_BOUTTON_CIRCUIT]) {
+        [self.delegate didTapCircuitButton:self];
+    } else if ([[self delegate] respondsToSelector:@selector(didTapActuButton:)] && [self.nomBoutton isEqualToString: F1_BOUTTON_ACTU]) {
+        [self.delegate didTapActuButton:self];
+    } else if ([[self delegate] respondsToSelector:@selector(didTapInfoButton:)] && [self.nomBoutton isEqualToString: F1_BOUTTON_INFO]) {
+        [self.delegate didTapInfoButton:self];
+    }
+}
+
+@end
