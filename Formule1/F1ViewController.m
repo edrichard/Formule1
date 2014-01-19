bf78846f4e86bee982066bfc8ac9e4f7e2824765
RICHARD Eddy <richard.eddy@live.fr>
2014-01-19 17:22:02 +0100

    Création splacheScreen.

--- Formule1/F1ViewController.m
+++ Formule1/F1ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "F1ViewController.h"
+#import "F1SplacheScreen.h"
+#import "F1HomeDashBoardViewController.h"
 
 @interface F1ViewController ()
+
+@property (nonatomic, strong) F1SplacheScreen *splachScreen;
 
 @end
 
@@ -17,7 +21,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.navigationController.navigationBarHidden = YES;
+    
+    self.splachScreen = [[F1SplacheScreen alloc] init];
+    self.splachScreen.logoImgName = @"logoF1.jpg";
+    self.splachScreen.accroche = @"La F1 au quotidien";
+    [self.splachScreen setup];
+    [self.view addSubview:self.splachScreen];
+    
+    NSInteger delay = 2;
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        self.splachScreen.alpha = 0;
+        F1HomeDashBoardViewController *viewHomeDashBoard = [F1HomeDashBoardViewController new];
+        [self.navigationController pushViewController:viewHomeDashBoard animated:YES];
+    });
 }
 
 - (void)didReceiveMemoryWarning
