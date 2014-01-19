fb06f3829850a1c824f1589ec2830c6146bfb827
RICHARD Eddy <richard.eddy@live.fr>
2014-01-19 16:37:29 +0100

    Fichier constant

---
+++ Formule1/F1Constant.h
@@ -0,0 +1,10 @@
+//
+//  F1Constant.h
+//  Formule1
+//
+//  Created by RICHARD Eddy on 19/01/2014.
+//  Copyright (c) 2014 richardeddy. All rights reserved.
+//
+
+#define SCREEN_WIDTH [[UIScreen mainScreen] bounds].size.width
+#define SCREEN_HEIGHT [[UIScreen mainScreen] bounds].size.height
